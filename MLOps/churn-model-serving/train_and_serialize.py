26ec3c4d77096e5b3ed37bdc8bc78d70a5d922f3
riddhikale <riddhiskale28@gmail.com>
2026-08-01 12:22:32 +0530

    initial libraries and implementation

---
+++ MLOps/churn-model-serving/train_and_serialize.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+import joblib
+import os
+
